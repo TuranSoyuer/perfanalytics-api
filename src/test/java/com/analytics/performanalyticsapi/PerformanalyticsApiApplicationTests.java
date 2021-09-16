01cbb0543bdd95e0e4911026b7e675fb5f4dfc2c
Turan Soyuer <soyuer.turan@gmail.com>
2021-09-16 20:06:53 +0300

    First commit.

---
+++ src/test/java/com/analytics/performanalyticsapi/PerformanalyticsApiApplicationTests.java
@@ -0,0 +1,13 @@
+package com.analytics.performanalyticsapi;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class PerformanalyticsApiApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
