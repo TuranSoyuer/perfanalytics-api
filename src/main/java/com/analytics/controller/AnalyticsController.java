9c1ec7a45aace872f912fc094b7b33e7084a41c3
Turan Soyuer <soyuer.turan@gmail.com>
2021-09-17 00:15:22 +0300

    Create api added.

--- src/main/java/com/analytics/controller/AnalyticsController.java
+++ src/main/java/com/analytics/controller/AnalyticsController.java
@@ -1,6 +1,7 @@
 package com.analytics.controller;
 
 import com.analytics.controller.input.AnalyticInput;
+import lombok.extern.java.Log;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.PostMapping;
@@ -8,15 +9,14 @@ import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
+@Log
 @RestController
 @RequestMapping("/api")
 public class AnalyticsController {
 
     @PostMapping("/analytic")
     public ResponseEntity createAnalytic(@RequestBody AnalyticInput analyticInput) {
-        analyticInput.toString();
-
-
+        log.info("createAnalytic HERE:" + analyticInput.toString());
         return ResponseEntity.ok(HttpStatus.OK);
     }
 }
