377fc9947c2327f409bad2d7ae095b0683af91e3
Turan Soyuer <soyuer.turan@gmail.com>
2021-09-16 23:27:25 +0300

    Pom edited.

--- src/main/java/com/analytics/Main.java
+++ src/main/java/com/analytics/Main.java
@@ -16,24 +16,14 @@
 
 package com.analytics;
 
-import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.beans.factory.annotation.Value;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.RequestMapping;
 
-import javax.sql.DataSource;
-
 @Controller
 @SpringBootApplication
 public class Main {
-
-  @Value("${spring.datasource.url}")
-  private String dbUrl;
-
-  @Autowired
-  private DataSource dataSource;
 
   public static void main(String[] args) throws Exception {
     SpringApplication.run(Main.class, args);
