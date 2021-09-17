fa35e162ce3801db95dd64a13f5c45d3e5cf65a2
Turan Soyuer <soyuer.turan@gmail.com>
2021-09-17 22:37:39 +0300

    Cross origin.

--- src/main/java/com/analytics/config/WebConfig.java
+++ src/main/java/com/analytics/config/WebConfig.java
@@ -2,15 +2,14 @@ package com.analytics.config;
 
 import org.springframework.context.annotation.Configuration;
 import org.springframework.web.servlet.config.annotation.CorsRegistry;
-import org.springframework.web.servlet.config.annotation.EnableWebMvc;
 import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
 
 @Configuration
-@EnableWebMvc
+//@EnableWebMvc
 public class WebConfig implements WebMvcConfigurer {
 
     @Override
     public void addCorsMappings(CorsRegistry registry) {
-        registry.addMapping("/api/**");
+        registry.addMapping("/**");
     }
 }
