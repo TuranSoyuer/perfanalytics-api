be6595491dcf535a67baf3879b5e614190e20bb6
Turan Soyuer <soyuer.turan@gmail.com>
2021-09-17 11:00:13 +0300

    Create entity dev.

--- src/main/java/com/analytics/repository/ResourceItem.java
+++ src/main/java/com/analytics/repository/ResourceItem.java
@@ -1,11 +1,11 @@
-package com.analytics.repository;
-
-import lombok.Data;
-
-@Data
-public class ResourceItem {
-    String name;
-    double duration;
-    String initiatorType;
-    double transferSize;
-}
+//package com.analytics.repository;
+//
+//import lombok.Data;
+//
+//@Data
+//public class ResourceItem {
+//    String name;
+//    double duration;
+//    String initiatorType;
+//    double transferSize;
+//}
