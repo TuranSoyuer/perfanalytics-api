2dce0b8131f52a398d97c42b880a0e77c3247cc6
Turan Soyuer <soyuer.turan@gmail.com>
2021-09-17 22:45:21 +0300

    Cross origin.

--- src/main/java/com/analytics/config/WebConfig.java
+++ src/main/java/com/analytics/config/WebConfig.java
@@ -10,6 +10,7 @@ public class WebConfig implements WebMvcConfigurer {
 
     @Override
     public void addCorsMappings(CorsRegistry registry) {
-        registry.addMapping("/**");
+        registry.addMapping("/**")
+                .allowedOrigins("https://performanceanalytics-app.herokuapp.com/");
     }
 }
